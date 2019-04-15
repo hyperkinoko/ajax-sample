1b1b8ec270d95c2fec0d0522f73da39a62345db9
kinoko Matumoto <superkinoko@icloud.com>
2019-04-16 08:31:34 +0900

    first app

--- src/main/java/net/kinokolabo/reservation/controller/HelloController.java
+++ src/main/java/net/kinokolabo/reservation/controller/HelloController.java
@@ -1,4 +1,12 @@
 package net.kinokolabo.reservation.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HelloController {
+    @GetMapping
+    public String getHello() {
+        return "hello";
+    }
 }
