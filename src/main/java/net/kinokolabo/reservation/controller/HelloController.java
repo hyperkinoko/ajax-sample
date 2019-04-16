b93f56c4a6fd2a4cc96c5b85dae1ea10cf8d48c9
kinoko Matumoto <superkinoko@icloud.com>
2019-04-16 14:57:59 +0900

    make RequestMapping

--- src/main/java/net/kinokolabo/reservation/controller/HelloController.java
+++ src/main/java/net/kinokolabo/reservation/controller/HelloController.java
@@ -1,12 +1,14 @@
 package net.kinokolabo.reservation.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
-    @GetMapping
+
+    @RequestMapping("/waiwai")
     public String getHello() {
-        return "hello";
+        return "わいわい";
     }
 }
