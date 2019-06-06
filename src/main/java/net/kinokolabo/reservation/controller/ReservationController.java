2012bd225860b0005a893751ab8b360e317424bc
kinoko Matumoto <superkinoko@icloud.com>
2019-06-06 23:24:56 +0900

    originを追加

--- src/main/java/net/kinokolabo/reservation/controller/ReservationController.java
+++ src/main/java/net/kinokolabo/reservation/controller/ReservationController.java
@@ -23,7 +23,7 @@ public class ReservationController {
     @Autowired
     OpenMapper openMapper;
 
-    @CrossOrigin(Origin="https://kinokodata.net")
+    @CrossOrigin(origins="https://kinokodata.net")
     @RequestMapping(value = "/open", method = RequestMethod.GET)
     public ArrayList<Open> getOpen(@RequestParam(defaultValue = "0") Integer year,
                                    @RequestParam(defaultValue = "0") Integer month,
