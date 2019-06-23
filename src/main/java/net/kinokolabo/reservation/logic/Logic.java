6c024be1ecfc31608f1000fef71977993099cd55
kinoko Matumoto <superkinoko@icloud.com>
2019-06-24 08:03:12 +0900

    save

--- src/main/java/net/kinokolabo/reservation/logic/Logic.java
+++ src/main/java/net/kinokolabo/reservation/logic/Logic.java
@@ -4,6 +4,10 @@ import com.sendgrid.Email;
 import net.kinokolabo.reservation.domain.JoinOrLeave;
 import net.kinokolabo.reservation.domain.Mail;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public class Logic {
 
     public boolean sendMail(String name, JoinOrLeave joinOrLeave, Email to) {
@@ -11,8 +15,13 @@ public class Logic {
         mail.setTo(to);
         mail.setFrom(new Email("kinokolabo-visit@kinokodata.net"));
         mail.setSubject("【もくもく会】" + name + "さんが" +joinOrLeave.getActionName() + "しました");
-        mail.setMessage("もくもく会に" + name + "さんが" +joinOrLeave.getActionName() + "しました");
-
+        String mess = LocalDateTime.now().format(DateTimeFormatter.ofPattern("H時M分、", Locale.JAPAN));
+        if(joinOrLeave == JoinOrLeave.JOIN) {
+            mess += "もくもく会に" + name + "さんが入室しました。";
+        } else {
+            mess += "もくもく会を" + name + "さんが退室しました。";
+        }
+        mail.setMessage(mess);
         return mail.send();
     }
 }
